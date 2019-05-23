e245ca47d9a3ce68ad8d70f05c21d5f8e5b34100
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-23 14:04:59 +0100

    6-54 Reading db on overridden event OnAppearing

--- TravelRecord/TravelRecord/HistoryPage.xaml.cs
+++ TravelRecord/TravelRecord/HistoryPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecord.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,18 @@ namespace TravelRecord
 		{
 			InitializeComponent ();
 		}
-	}
+
+        // Implementing OnAppearing so as to refresh the data on the page whenever navigated back-to
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);     // use previously established location
+
+            conn.CreateTable<Post>();                       // create table (only) if non-existent
+            var posts = conn.Table<Post>().ToList();        // get list of Post objects (ie records)
+
+            conn.Close();                                   // NB remembering to close connection!
+        }
+    }
 }
