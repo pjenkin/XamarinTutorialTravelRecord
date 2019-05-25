a1df959e47a164121c7b8c494c89ab31b344585d
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-25 16:15:33 +0100

    8-65 Constants class added (with FourSquare API URL details) and Venue class started.

---
+++ TravelRecord/TravelRecord/Helpers/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecord.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+
+        // https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD from https://developer.foursquare.com/docs/api/configuration/authentication
+        // Use String.Format presently 8-65
+
+        public const string CLIENT_ID = "G1JNW5Z5HBYZTY1OKGP2CKQMMA12I1ISBFRUGKANPBMRDJTI";     // ought not to be uploading these to online public repo, but hey nvmd
+        public const string CLIENT_SECRET = "3DKYDIHFHVJGM3PNBMPPUDCNKU3LPL0BFKSLB3FZRBGCNYAA";
+        // From FourSquare API developer console, having used 'Create App' (in 8-64)
+        
+    }
+}
---
+++ TravelRecord/TravelRecord/Model/Venue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TravelRecord.Helpers;     // We defined Constants class in here 8-65
+
+namespace TravelRecord.Model
+{
+    public class Venue
+    {
+        public static void GenerateURL(double latitude, double longitude)
+        {
+            string url = String.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));            
+        }
+    }
+}
