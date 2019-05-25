115ff82d764d1f39ed6251647b0ff1f6f64feece
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-25 20:59:13 +0100

    8-71 Venue data saved from deserialized API response to local posts db

--- TravelRecord/TravelRecord/Model/Post.cs
+++ TravelRecord/TravelRecord/Model/Post.cs
@@ -12,5 +12,23 @@ namespace TravelRecord.Model
 
         [MaxLength(250)]
         public string ExperienceDescription { get; set; }        
+
+
+        public string VenueName { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Address { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public int Distance { get; set; }
+
+
+        // I'm not sure why we don't just have a Venue ID, CategoryID &c in here (TODO??)
     }
 }
--- TravelRecord/TravelRecord/NewTravelPage.xaml.cs
+++ TravelRecord/TravelRecord/NewTravelPage.xaml.cs
@@ -14,13 +14,13 @@ using TravelRecord.Logic;
 
 namespace TravelRecord
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewTravelPage : ContentPage
-	{
-		public NewTravelPage ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
 
         // override here - this called whenever page loaded by new user
         protected async override void OnAppearing()
@@ -36,34 +36,59 @@ namespace TravelRecord
 
         private void SaveToolbarItem_Clicked(object sender, EventArgs e)
         {
-            // insert Post record into db
-            Post post = new Post()
-            {
-                ExperienceDescription = experienceDescriptionEntry.Text
-                // Id set automatically
-            };          // NB initialising the new instance's members thus in a terminated-block
+            try
+            { 
+                var selectedVenue = venueListView.SelectedItem as Venue;    // get clicked Venue as object of Venue class
 
-            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                var firstCategory = selectedVenue.categories.FirstOrDefault();      // approved, safe, way of getting first of zero to many categories of venue
+
+                // insert Post record into db
+                Post post = new Post()
+                {
+                    ExperienceDescription = experienceDescriptionEntry.Text,
+                    // use new text entry box value for description (otherwise use deserialised JSON data fields from API response)
+                    // Id set automatically
+                    // CategoryId = selectedVenue.categories[0].id;        // just get first category - not safe way though
+                    CategoryId = firstCategory.id,
+                    CategoryName = selectedVenue.location.address,
+                    Address = selectedVenue.location.address,
+                    Latitude = selectedVenue.location.lat,
+                    Longitude = selectedVenue.location.lng,
+                    Distance = selectedVenue.location.distance,
+                    VenueName = selectedVenue.name
+
+                };          // NB initialising the new instance's members thus in a terminated-block
+
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
                 // Use db location class member defined earlier in 6-49
                 // Since SQLiteConnection (qv) is implementing IDisposable, we can, with a 'using' statement, 
                 // safely leave out connection.Close call as Dispose will be automatically called
+                {
+                    conn.CreateTable<Post>();
+
+                    int numRows = conn.Insert(post);  // type can also be used automatically to deduce to which table to insert
+
+                    // conn.Close();       // close the connection as well - not needed if 'using' SQLiteConnection
+
+                    if (numRows > 0)
+                    {
+                        // diagnostic alert
+                        DisplayAlert("Success", "Record successfully inserted", "OK");
+                    }
+                    else
+                    {
+                        DisplayAlert("Failure", "No record inserted", "OK");
+                    }
+                }       // end of 'using' statement block
+            }
+            catch (NullReferenceException nre)      // e.g. in case there's no category (null)
             {
-                conn.CreateTable<Post>();
 
-                int numRows = conn.Insert(post);  // type can also be used automatically to deduce to which table to insert
+            }
+            catch (Exception ex)                // for more general exceptions that specifically null exceptions
+            {
 
-                // conn.Close();       // close the connection as well - not needed if 'using' SQLiteConnection
-
-                if (numRows > 0)
-                {
-                    // diagnostic alert
-                    DisplayAlert("Success", "Record successfully inserted", "OK");
-                }
-                else
-                {
-                    DisplayAlert("Failure", "No record inserted", "OK");
-                }
-            }       // end of 'using' statement block
-        }
+            }
+        } 
     }
 }
