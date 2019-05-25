2279435ba5f22e65745292024f9d84c773044328
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-25 17:43:29 +0100

    7-68 jsonutils.com, with FourSquare API JSON output sample, used to determine & add (ie paste in) classes for deserialisation of JSON

--- TravelRecord/TravelRecord/Logic/VenueLogic.cs
+++ TravelRecord/TravelRecord/Logic/VenueLogic.cs
@@ -9,11 +9,11 @@ namespace TravelRecord.Logic
 {
     public class VenueLogic
     {
-        public async static Task <List<Venue>> GetVenues(double latitude, double longitude)      // return a list of Venues, for a given location
+        public async static Task <List<VenueRoot>> GetVenues(double latitude, double longitude)      // return a list of Venues, for a given location
         {       // had to be static as 'await' used below, and had to be Task<> as returning non-void in an async
-            List<Venue> venues = new List<Venue>();
+            List<VenueRoot> venues = new List<VenueRoot>();
 
-            var url = Venue.GenerateURL(latitude, longitude);
+            var url = VenueRoot.GenerateURL(latitude, longitude);
 
             using (HttpClient client = new HttpClient())
             {
--- TravelRecord/TravelRecord/Model/Venue.cs
+++ TravelRecord/TravelRecord/Model/Venue.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using TravelRecord.Helpers;     // We defined Constants class in here 8-65
 
 namespace TravelRecord.Model
 {
-    public class Venue
+    public class VenueRoot
     {
         public static string GenerateURL(double latitude, double longitude)
         {
@@ -12,4 +13,78 @@ namespace TravelRecord.Model
             return url;
         }
     }
+    /*
+    public class Meta
+    {
+        public int code { get; set; }
+        public string requestId { get; set; }
+    }
+    // not needed
+    */
+    public class LabeledLatLng
+    {
+        public string label { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public IList<LabeledLatLng> labeledLatLngs { get; set; }
+        public int distance { get; set; }
+        public string cc { get; set; }
+        public string country { get; set; }
+        public IList<string> formattedAddress { get; set; }
+        public string address { get; set; }
+        public string postalCode { get; set; }
+        public string neighborhood { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string crossStreet { get; set; }
+    }
+
+    public class Icon
+    {
+        public string prefix { get; set; }
+        public string suffix { get; set; }
+    }
+
+    public class Category
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string pluralName { get; set; }
+        public string shortName { get; set; }
+        public Icon icon { get; set; }
+        public bool primary { get; set; }
+    }
+
+    public class Venue
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Location location { get; set; }
+        public IList<Category> categories { get; set; }
+        public string referralId { get; set; }
+        public bool hasPerk { get; set; }
+    }
+
+    public class Response
+    {
+        public IList<VenueRoot> venues { get; set; }
+        /*  public bool confident { get; set; } */ // not needed
+    }
+    /*
+    public class Example
+    {
+        public Meta meta { get; set; }
+        public Response response { get; set; }
+    }
+    // not needed
+    */
+
+
+
 }
