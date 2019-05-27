588886dc95416ceeb66399465854beeb40b0cef4
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-27 16:47:02 +0100

    10-79 Where LINQ statement used to filter in order to count only certain records

--- TravelRecord/TravelRecord/ProfilePage.xaml.cs
+++ TravelRecord/TravelRecord/ProfilePage.xaml.cs
@@ -29,7 +29,18 @@ namespace TravelRecord
                 var categories = (from p in postTable
                                   orderby p?.CategoryId
                                   select p?.CategoryName ?? "No category given").Distinct().ToList();
-                    // don't show duplicated category name values (null conditional and coalescing added by PNJ)
+                // don't show duplicated category name values (null conditional and coalescing added by PNJ)
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();        // make key/value dictionary of tally counts of categories
+
+                foreach(var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count;               // LINQ used to count
+
+                    categoriesCount.Add(category, count);                   // add dictionary entry
+                }
 
                 postCountLabel.Text = postTable.Count.ToString();           // set text to number of posts
             }
