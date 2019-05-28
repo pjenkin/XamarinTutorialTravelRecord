afb0991f1f2f82fe90d3004efe8bb58b8fe46492
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-28 15:28:57 +0100

    11-87 Insert record to Azure db table on register added; also edited portable csproj to stop packages.config deploy error

---
+++ TravelRecord/TravelRecord/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecord.Model
+{
+    // class to represent a user
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User()
+        {
+            // blank constructor with no parameters
+        }
+
+        /// <summary>
+        ///  Constructor for User object
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
--- TravelRecord/TravelRecord/RegisterPage.xaml.cs
+++ TravelRecord/TravelRecord/RegisterPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecord.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,15 +17,24 @@ namespace TravelRecord
             InitializeComponent();
         }
 
-        private void RegisterButton_Clicked(object sender, EventArgs e)
+        private async void RegisterButton_Clicked(object sender, EventArgs e)
         {
             if (password.Text == confirmPasswordEntry.Text)
             {
                 // register the user
+                // register the user
+                User user = new User()
+                {
+                    Email = email.Text,
+                    Password = password.Text
+                    // Id filled in automatically
+                };
+
+                await App.MobileService.GetTable<User>().InsertAsync(user);
             }
             else
             {
-                DisplayAlert("Error", "Passwords don't match", "OK");
+                await DisplayAlert("Error", "Passwords don't match", "OK");
             }
         }
 
