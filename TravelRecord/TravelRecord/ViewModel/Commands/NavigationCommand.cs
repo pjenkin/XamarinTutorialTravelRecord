1a848ad12d4bf5cddb31fb215021aa5e39d466d7
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-29 13:49:13 +0100

    12-101 NavigationCommand (ICommand) and HomeViewModel added to portable project. Initial setup.

---
+++ TravelRecord/TravelRecord/ViewModel/Commands/NavigationCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace TravelRecord.ViewModel.Commands
+{
+    public class NavigationCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;        // boilerplate
+        public HomeVM HomeViewModel { get; set; }           // use our hand-written ViewModel class
+
+        public NavigationCommand(HomeVM homeVM)
+        {
+            HomeViewModel = homeVM;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+            return true;                    // no checks needed in this case
+        }
+
+        public void Execute(object parameter)
+        {
+            // throw new NotImplementedException();
+            // functionality to be defined in ViewModel (cf HomeVM in ViewModel folder)
+            HomeViewModel.Navigate();
+        }
+    }
+}
---
+++ TravelRecord/TravelRecord/ViewModel/HomeVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecord.ViewModel.Commands;
+
+namespace TravelRecord.ViewModel
+{
+    public class HomeVM
+    {
+        NavigationCommand NavCommand;           // our own ICommand implementation 12-101
+
+        public HomeVM()
+        {
+            NavCommand = new NavigationCommand(this);
+        }
+
+        // hand-written bespoke method for navigating via ICommand implementation NavigationCommand
+        public void Navigate()
+        {
+
+        }
+    }
+}
